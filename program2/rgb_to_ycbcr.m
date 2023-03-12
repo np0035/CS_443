6f70c844fc0b7ed827781d74799a6d4f11157307
Nicholas Pittman <np0035@uah.edu>
2023-03-12 18:13:53 -0500

    Added comments

--- program2/rgb_to_ycbcr.m
+++ program2/rgb_to_ycbcr.m
@@ -18,9 +18,14 @@ function output_img = rgb_to_ycbcr(input_filename)
     for r = 1:size(input_img, 1)
         for c = 1:size(input_img, 2)
 
-            % Get the current pixel into a form we can work with
+            % Get the current pixel into a form we can work with and
+            % normalize it
             px_rgb = double(reshape(input_img(r,c,:), [3, 1])) ./ 256;
+
+            % Perform the conversion
             px_yCbCr = reshape(((convert_matrix * px_rgb) + [0; 0.5; 0.5]) .* 256, [1, 1, 3]);
+
+            % Store the resulting pixel into the output image
             output_img(r,c,:) = px_yCbCr;
         end
     end

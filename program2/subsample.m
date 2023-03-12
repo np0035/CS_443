d182a8bee6d6e028638812a021170133efd7a905
Nicholas Pittman <np0035@uah.edu>
2023-03-12 18:35:16 -0500

    Added cases

--- program2/subsample.m
+++ program2/subsample.m
@@ -1,4 +1,12 @@
 function output_img = subsample(input_img, scheme)
-    
+    switch scheme
+        case [4 2 2]
+
+        case [4 1 1]
+            
+        otherwise
+            fprintf("%s is not a supported subsampling scheme.\n", scheme)
+            output_img = input_img;
+    end
 end
 
